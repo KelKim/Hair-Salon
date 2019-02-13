ac507bedff1f9e75679da732cb88738866f7bbea
Kelvins <neguskel@gmail.com>
2019-02-13 11:57:06 +0300

    deleted details on the client and stylist.java file to fit the database table

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -3,34 +3,18 @@ import java.util.List;
 import org.sql2o.*;
 
 public class Client {
-    private String clientFirstName;
-    private String clientLastName;
-    private String clientPhoneNo;
-    private String clientEmail;
-    private int id;
-    private int stylistId;
+    private String clientName;
 
 
-    public Client(String clientFirstName, String clientLastName, String clientPhoneNo, String clientEmail, int stylistId){
-        this.clientFirstName = clientFirstName;
-        this.clientLastName = clientLastName;
-        this.clientPhoneNo = clientPhoneNo;
-        this.clientEmail = clientEmail;
+    public Client(String clientName){
+        this.clientName = clientName;
         this.stylistId = stylistId;
     }
 
-    public String getClientFirstName(){
-        return clientFirstName;
+    public String getClientName(){
+        return clientName;
     }
-    public String getClientLastName(){
-        return clientLastName;
-    }
-    public String getClientPhoneNo(){
-        return clientPhoneNo;
-    }
-    public String getClientEmail(){
-        return clientEmail;
-    }
+    
     public int getId() {
         return id;
     }
@@ -49,7 +33,7 @@ public class Client {
 
     }
     public static List<Client> all() {
-        String sql = "SELECT id, clientFirstName, clientLastName, clientPhoneNo, clientEmail, stylistId FROM clients";
+        String sql = "SELECT id, clientName, stylistId FROM clients";
         try (Connection con = DB.sql2o.open()) {
             return con.createQuery(sql).executeAndFetch(Client.class);
         }
@@ -61,10 +45,7 @@ public class Client {
             return false;
         } else {
             Client newClient = (Client) otherClient;
-            return this.getClientFirstName().equals(newClient.getClientFirstName()) &&
-                    this.getClientLastName().equals(newClient.getClientLastName()) &&
-                    this.getClientPhoneNo().equals(newClient.getClientPhoneNo()) &&
-                    this.getClientEmail().equals(newClient.getClientEmail()) &&
+            return this.getClientName().equals(newClient.getClientName()) &&
                     this.getId() == newClient.getId()  &&
                     this.getStylistId() == newClient.getStylistId();
         }
@@ -72,12 +53,9 @@ public class Client {
 
     public void save() {
         try(Connection con = DB.sql2o.open()) {
-            String sql = "INSERT INTO clients (clientFirstName, clientLastName, clientPhoneNo, clientEmail, stylistId) VALUES (:clientFirstName, :clientLastName, :clientPhoneNo, :clientEmail, :stylistId)";
+            String sql = "INSERT INTO clients (clientName, stylistId) VALUES (:clientName, :stylistId)";
             this.id = (int) con.createQuery(sql, true)
-                    .addParameter("clientFirstName", this.clientFirstName)
-                    .addParameter("clientLastName", this.clientLastName)
-                    .addParameter("clientPhoneNo", this.clientPhoneNo)
-                    .addParameter("clientEmail", this.clientEmail)
+                    .addParameter("clientName", this.clientName)
                     .addParameter("stylistId", this.stylistId)
                     .executeUpdate()
                     .getKey();
--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -2,47 +2,22 @@ import org.sql2o.*;
 import java.util.List;
 
 public class Stylist {
-    private String firstName;
-    private String secondName;
-    private String lastName;
-    private String phoneNo;
-    private String idNo;
-    private String email;
+    private String stylistName;
     private int id;
 
-    public Stylist(String firstName, String secondName, String lastName, String phoneNo, String idNo, String email){
-        this.firstName = firstName;
-        this.secondName = secondName;
-        this.lastName = lastName;
-        this.phoneNo = phoneNo;
-        this.idNo = idNo;
-        this.email = email;
+    public Stylist(String stylistName){
+        this.stylistName = stylistName;
     }
 
-    public String getFirstName(){
-        return firstName;
-    }
-    public String getSecondName(){
-        return secondName;
-    }
-    public String getLastName(){
-        return lastName;
-    }
-    public String getPhoneNo(){
-        return phoneNo;
-    }
-    public String getIdNo(){
-        return idNo;
-    }
-    public String getEmail(){
-        return email;
+    public String getStylistName(){
+        return stylistName;
     }
     public int getId(){
         return id;
     }
 
     public static List<Stylist> all(){
-        String sql = "SELECT id, firstName, secondName, lastName, phoneNo, idNo, email FROM stylists";
+        String sql = "SELECT id, stylistName FROM stylists";
         try(Connection con = DB.sql2o.open()) {
             return con.createQuery(sql).executeAndFetch(Stylist.class);
         }
@@ -61,25 +36,15 @@ public class Stylist {
             return false;
         } else {
             Stylist newStylist = (Stylist) otherStylist;
-            return this.getFirstName().equals(newStylist.getFirstName()) &&
-                    this.getSecondName().equals(newStylist.getSecondName()) &&
-                    this.getLastName().equals(newStylist.getLastName()) &&
-                    this.getPhoneNo().equals(newStylist.getPhoneNo()) &&
-                    this.getIdNo().equals(newStylist.getIdNo()) &&
-                    this.getEmail().equals(newStylist.getEmail()) &&
+            return this.getStylistName().equals(newStylist.getStylistName()) &&
                     this.getId() == newStylist.getId();
         }
     }
     public void save() {
         try(Connection con = DB.sql2o.open()) {
-            String sql = "INSERT INTO stylists (firstName, secondName, lastName, phoneNo, idNo, email) VALUES (:firstName, :secondName, :lastName, :phoneNo, :idNo, :email)";
+            String sql = "INSERT INTO stylists (stylistName) VALUES (:stylistName)";
             this.id = (int) con.createQuery(sql, true)
-                    .addParameter("firstName", this.firstName)
-                    .addParameter("secondName", this.secondName)
-                    .addParameter("lastName", this.lastName)
-                    .addParameter("phoneNo", this.phoneNo)
-                    .addParameter("idNo", this.idNo)
-                    .addParameter("email", this.email)
+                    .addParameter("stylistName", this.stylistName)
                     .executeUpdate()
                     .getKey();
         }
